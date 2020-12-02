125a748eff2388e3e70c528dde1afcbcca81c594
clemencelarrouy <75069054+clemencelarrouy@users.noreply.github.com>
2020-12-02 14:10:55 +0100

    Add files via upload

--- style/jqueryMPC.js
+++ style/jqueryMPC.js
@@ -1,28 +1,34 @@
-// Jquery Mon Petit Commerce 
-$(document).ready(function() {
-    console.log('page html chargé');
-    
+// Jquery Mon Petit Commerce
 
-//////////////////////////////////////////////// MATRICE FICHE PRODUIT 
+// pour eviter d'avoir à charger dans toutes les pages tous les fichiers Jquery et Javascripts afin d'optimiser la vitesse de chargement, le script "Imprimer la page" a été directement placé dans le footer.
 
-    $(document).ready(function(){
-      $("#imgnav").find(".imageclickable").attr('width',100);
-    });
-  
-    $(document).on('click','.imageclickable',function(){
-      //$(this).width(600);
-      $("#imggrande").empty();
-      $("#imggrande").append("<input type='image'  alt='Login' class='imageclickable' src='"+$(this).attr('src')+"'>");
-      $("#imggrande").find("input").attr("width",400);
-  
-    });
+//chargement de la page
+$(document).ready(function () {
+  console.log("page html chargé");
 
-//// onglet description composition 
-$("#monOnglet").tabs();
+  //changement photo sur les fiches produits
+  $(document).ready(function () {
+    $("#imgnav").find(".imageclickable").attr("width", 100);
+  });
 
-/// date picker 
-$('input.date_calendrier').datepicker(); 
+  $(document).on("click", ".imageclickable", function () {
+    //$(this).width(600);
+    $("#imggrande").empty();
+    $("#imggrande").append(
+      "<input type='image'  alt='Login' class='imageclickable' src='" +
+        $(this).attr("src") +
+        "'>"
+    );
+    $("#imggrande").find("input").attr("width", 400);
+  });
 
+  //// onglet description composition dans les fiches produits
+  $("#monOnglet").tabs();
 
-
+  /// date picker dans la page personal shopper
+  $("input.date_calendrier").datepicker();
 });
+
+// top
+
+$("#demo").badge(4, "top");
